0a25064899a676ca23003ea3b11e4538dea2daa0
marcos <aaarrrhhheee123@gmail.com>
2025-05-12 14:22:32 -0300

    Fix vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/bunch-pay-front/', // esto es clave
   plugins: [react()],
-})
+});
+
